f625519ac7e9a5803e1b1536d6c5f07cfe8fa3e5
Ricardo Gomes <ipsiart@gmail.com>
2020-05-09 22:04:59 +0100

    Adds BulbSwitcher.java [6 ms]

---
+++ src/main/java/exercises/array/BulbSwitcher.java
@@ -0,0 +1,18 @@
+package exercises.array;
+
+public class BulbSwitcher {
+    public int numTimesAllBlue(int[] light) {
+        return numTimesAllBlue( light, Integer.MIN_VALUE, 0 );
+    }
+
+    private int numTimesAllBlue(int[] light, int maxValue, int i) {
+        if (i > light.length) {
+            return 0;
+        } else {
+            int newIndex = light[i];
+            int newMaxValue = Math.max( newIndex, maxValue );
+
+            return (newMaxValue == i + 1 ? 1 : 0) + numTimesAllBlue( light, newMaxValue, i + 1 );
+        }
+    }
+}
