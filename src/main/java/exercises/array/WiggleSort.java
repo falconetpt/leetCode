80cd7524118758f6736c2ca8269d06dd457401e7
Ricardo Gomes <ipsiart@gmail.com>
2020-05-14 23:34:39 +0100

    Adds SmallestRepUnitDivBy/k.java [1634 ms]

---
+++ src/main/java/exercises/array/WiggleSort.java
@@ -0,0 +1,25 @@
+package exercises.array;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class WiggleSort {
+    public void wiggleSort(int[] nums) {
+        LinkedList<Integer> sortedNums = Arrays.stream(nums).boxed()
+                .sorted()
+                .collect( Collectors.toCollection( LinkedList::new ));
+
+        wiggleSort( sortedNums, nums, true, 0 );
+    }
+
+    private void wiggleSort(LinkedList<Integer> sortedNums, int[] nums, boolean first, int i) {
+        if (!sortedNums.isEmpty()) {
+            int nextElement = first ? sortedNums.pollFirst() : sortedNums.pollLast();
+
+            nums[i] = nextElement;
+
+            wiggleSort( sortedNums, nums, !first, i + 1 );
+        }
+    }
+}
