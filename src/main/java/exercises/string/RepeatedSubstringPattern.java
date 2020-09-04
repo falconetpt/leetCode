6ce9f12edca43d51986e3aa319040503d9ac2538
Ricardo Gomes <ipsiart@gmail.com>
2020-09-04 09:36:38 +0100

    RepeatedSubstringPattern [277 ms]

--- src/main/java/exercises/string/RepeatedSubstringPattern.java
+++ src/main/java/exercises/string/RepeatedSubstringPattern.java
@@ -1,4 +1,20 @@
 package exercises.string;
 
 public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(final String s) {
+        int size = s.length();
+        return repeatedSubstringPattern( s, 0, size, "" );
+    }
+
+    public boolean repeatedSubstringPattern(final String s, final int index,
+                                            final int size, final String substring) {
+        if (index >= size - 1) {
+            return false;
+        } else {
+            String newString = substring + s.charAt( index );
+
+            return (size % newString.length() == 0 && s.split( newString ).length == 0)
+                    || repeatedSubstringPattern( s, index + 1, size, newString );
+        }
+    }
 }
--- src/main/java/exercises/string/StreamChecker.java
+++ src/main/java/exercises/string/StreamChecker.java
@@ -1,4 +1,35 @@
 package exercises.string;
 
-public class StringChecker {
+import java.util.Arrays;
+import java.util.Objects;
+
+public class StreamChecker {
+    private StreamCheckerTrie streamCheckerTrie;
+
+    public StreamChecker(String[] words) {
+        streamCheckerTrie = new StreamCheckerTrie();
+        Arrays.stream(words).forEach( streamCheckerTrie::addWord );
+    }
+
+    public boolean query(char letter) {
+        return streamCheckerTrie.search( letter );
+    }
+
+    private class StreamCheckerTrie {
+        private StreamCheckerTrie[] streamCheckerTries;
+
+        public StreamCheckerTrie() {
+            streamCheckerTries = new StreamCheckerTrie[26];
+        }
+
+        public void addWord(final String word) {
+            int index = word.charAt( word.length() - 1 ) - 'a';
+            if (streamCheckerTries[index] == null) streamCheckerTries[index] = new StreamCheckerTrie();
+        }
+
+        public boolean search(final char word) {
+            int index = word - 'a';
+            return Objects.nonNull(streamCheckerTries[index]);
+        }
+    }
 }
