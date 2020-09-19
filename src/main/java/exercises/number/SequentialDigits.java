281496f4450e0399494039b27d8b4d0c6e78b402
Ricardo Gomes <ipsiart@gmail.com>
2020-09-19 11:24:27 +0100

    Sequential Digits [4 ms]

---
+++ src/main/java/exercises/number/SequentialDigits.java
@@ -0,0 +1,39 @@
+package exercises.number;
+
+import scala.Int;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class SequentialDigits {
+    public List<Integer> sequentialDigits(int low, int high) {
+        int lowPotence = String.valueOf( low ).length();
+        int highPotence = String.valueOf( high ).length();
+
+        List<Integer> values = generateNumbers(lowPotence, highPotence);
+
+        return values.stream()
+                .filter( x -> x >= low && x <= high )
+                .collect( Collectors.toList());
+    }
+
+    private List<Integer> generateNumbers(int lowPotence, int highPotence) {
+        return IntStream.rangeClosed( lowPotence, highPotence ).boxed()
+                .filter( n -> n < 10 )
+                .flatMap( n -> generateSequentialNumbers( n ).stream() )
+                .collect( Collectors.toList() );
+    }
+
+    private List<Integer> generateSequentialNumbers(int n) {
+        String range = "123456789";
+
+        return IntStream.rangeClosed( 0, range.length() - n ).boxed()
+                .map( s -> Integer.valueOf( range.substring( s, s + n ) ) )
+                .collect( Collectors.toList());
+    }
+
+
+}
