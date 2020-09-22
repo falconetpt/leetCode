72b2fddca86856e5f950554b1d95f4601d87f2a3
Ricardo Gomes <ipsiart@gmail.com>
2020-09-22 21:55:09 +0100

    MajorityElement3 [11 ms]

---
+++ src/main/java/exercises/array/UniquePaths3.java
@@ -0,0 +1,56 @@
+package exercises.array;
+
+import java.awt.Point;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class UniquePaths3 {
+    public int uniquePathsIII(final int[][] grid) {
+        final Set<Point> zeroPoints = IntStream.range( 0, grid.length ).boxed()
+                .flatMap(
+                        x -> IntStream.range( 0, grid[x].length ).boxed()
+                                .map( y -> new Point( x, y ) )
+                )
+                .collect( Collectors.toSet() );
+
+        final List<Point> moves = Stream.of(
+                new Point( 0, 1 ),
+                new Point( 0, -1 ),
+                new Point( 1, 0 ),
+                new Point( -1, 0 )
+        ).collect( Collectors.toList());
+
+        final Point startPoint = IntStream.range( 0, grid.length ).boxed()
+                .flatMap(
+                        x -> IntStream.range( 0, grid[x].length ).boxed()
+                                .map( y -> new Point( x, y ) )
+                )
+                .filter( x -> grid[x.x][x.y] == 1 )
+                .findFirst()
+                .orElse( null );
+
+        final Point finalPoint = IntStream.range( 0, grid.length ).boxed()
+                .flatMap(
+                        x -> IntStream.range( 0, grid[x].length ).boxed()
+                                .map( y -> new Point( x, y ) )
+                )
+                .filter( x -> grid[x.x][x.y] == 2 )
+                .findFirst()
+                .orElse( null );
+
+        // DFS application to the starting point
+
+        return uniquePathsIII( startPoint, finalPoint, moves, zeroPoints );
+    }
+
+    private int uniquePathsIII(final Point startPoint,
+                               final Point finalPoint,
+                               final List<Point> moves,
+                               final Set<Point> zeroPoints) {
+        return 0;
+    }
+}
--- src/main/java/exercises/number/SequentialDigits.java
+++ src/main/java/exercises/number/SequentialDigits.java
@@ -1,12 +1,8 @@
 package exercises.number;
 
-import scala.Int;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class SequentialDigits {
     public List<Integer> sequentialDigits(int low, int high) {
