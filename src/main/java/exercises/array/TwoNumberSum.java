37fcfad3dea7201676288a3c4603315c9a78cfed
Ricardo Gomes <ipsiart@gmail.com>
2020-11-27 00:26:14 +0000

    TwoSum

---
+++ src/main/java/exercises/array/TwoNumberSum.java
@@ -0,0 +1,23 @@
+package exercises.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class TwoNumberSum {
+    public int[] twoSum(int[] nums, int target) {
+        final Map<Integer, Integer> numberLastIndex = new HashMap<>(  );
+
+        // O (n) time | O(n) space
+        IntStream.range( 0, nums.length )
+                .forEach( i -> numberLastIndex.put( nums[i], i )  );
+
+        // O(n) time
+        return IntStream.range( 0, nums.length ).boxed()
+                .filter( i -> !i.equals( numberLastIndex.getOrDefault( target - nums[i], i ) ) )
+                .map( i -> new int[] { i, numberLastIndex.getOrDefault( target - nums[i], i ) } )
+                .findFirst()
+                .orElse( new int[] {} );
+    }
+}
---
+++ src/main/java/exercises/number/CanWin.java
@@ -0,0 +1,20 @@
+package exercises.number;
+
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class CanWin {
+//    public boolean divisorGame(int N) {
+//        return
+//    }
+//
+//    public void divisorGame(int N, Map<Integer, boolean[]> cache, int count) {
+//        if (N > 0) {
+//            IntStream.range( 1, N )
+//                    .filter( x -> N % x == 0 )
+//                    .peek( x -> divisorGame( N - x, cache, count + 1))
+//                    .peek( x -> cache.putIfAbsent( x ) );
+//
+//        }
+//    }
+}
