29c13337ea3ed69fb66de211f17260ca2e0a07ae
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2021-06-20 11:50:04 +0100

    Swimming in raising water

--- src/test/java/exercises/string/TitleToNumberTest.java
+++ src/test/java/exercises/string/TitleToNumberTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TitleToNumberTest {
-    private TitleToNumber titleToNumber = new TitleToNumber();
+    private final TitleToNumber titleToNumber = new TitleToNumber();
 
     @Test
     @DisplayName( "Scenario 1 - Simple Letter A to 1" )
