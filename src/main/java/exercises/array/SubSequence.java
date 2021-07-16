8a98e6e23ec9969def7f6725ddf5bac8ad0345c1
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2021-07-16 18:17:11 +0100

    isSubsequence

---
+++ src/main/java/exercises/array/SubSequence.java
@@ -0,0 +1,19 @@
+package exercises.array;
+
+public class SubSequence {
+  public boolean isSubsequence(String s, String t) {
+    return isSubsequence(s, t, 0, 0);
+  }
+
+  private boolean isSubsequence(final String s, final String t, final int sIndex, final int tIndex) {
+    if (sIndex >= s.length()) {
+      return true;
+    } else if (tIndex >= t.length()) {
+      return false;
+    } else {
+      return s.charAt(sIndex) == t.charAt(tIndex)
+        ? isSubsequence(s, t, sIndex + 1, tIndex + 1)
+        : isSubsequence(s, t, sIndex, tIndex + 1);
+    }
+  }
+}
