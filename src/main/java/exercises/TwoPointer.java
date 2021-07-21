c378ad16bd6f77d0f9ecd4bb10a0a1498931c314
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2021-07-21 12:47:34 +0100

    flip and invert image

---
+++ src/main/java/exercises/TwoPointer.java
@@ -0,0 +1,15 @@
+package exercises;
+
+public class TwoPointer {
+  public int[][] flipAndInvertImage(int[][] image) {
+    final var result = new int[image.length][image.length];
+
+    for (int i = 0; i < image.length; i++) {
+      for (int j = 0; j < image[i].length; j++) {
+        result[i][j] = image[i][image.length - 1 - j] ^ 1;
+      }
+    }
+
+    return result;
+  }
+}
