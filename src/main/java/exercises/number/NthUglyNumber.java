5aa3abc2f1bbc0caff03b7fda731f9fd50ba6fc8
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2021-10-14 23:05:28 +0100

    nth ugly number

---
+++ src/main/java/exercises/number/NthUglyNumber.java
@@ -0,0 +1,41 @@
+package exercises.number;
+
+import java.util.HashMap;
+
+public class NthUglyNumber {
+  public int nthUglyNumber(int n) {
+    final var result = new HashMap<Integer, Boolean>();
+    result.put(1, true);
+
+    var number = 1;
+    var valid = 1;
+
+    while (valid <= n) {
+      if (isUgly(number, result)) {
+        result.put(number, true);
+        valid++;
+      } else {
+        result.put(number, false);
+      }
+      number++;
+    }
+
+    return number - 1;
+  }
+
+  private boolean isUgly(final int number,
+                         final HashMap<Integer, Boolean> result) {
+    if (result.containsKey(number)) {
+      return result.get(number);
+    } else {
+      if (number % 2 == 0) {
+        return isUgly(number / 2, result);
+      } else if (number % 3 == 0) {
+        return isUgly(number / 3, result);
+      } else if (number % 5 == 0) {
+        return isUgly(number / 5, result);
+      }
+      return false;
+    }
+  }
+}
