41c1a8936c97d9f0c59437d170fe799f98a1e56b
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2021-10-17 13:22:55 +0100

    3 sum

--- src/main/java/exercises/blind/ThreeSum.java
+++ src/main/java/exercises/blind/ThreeSum.java
@@ -1,11 +1,39 @@
 package exercises.blind;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class ThreeSum {
   public List<List<Integer>> threeSum(final int[] nums) {
     Arrays.sort(nums);
+    final var result = new HashSet<List<Integer>>();
 
+    for (int i = 0; i < nums.length; i++) {
+      final var base = nums[i];
+      var start = i + 1;
+      var end = nums.length - 1;
+
+      while (start < end) {
+        final var startNum = nums[start];
+        final var endNum = nums[end];
+        final var sum = base + startNum + endNum;
+
+        if (sum == 0) {
+          result.add(List.of(base, startNum, endNum));
+          start++;
+        } else {
+          if (sum > 0) {
+            end--;
+          } else {
+            start++;
+          }
+        }
+      }
+
+    }
+
+    return new ArrayList<>(result);
   }
 }
