abb601215fcbc845ed5d3a31fe2d6103f421bae5
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2021-11-22 22:36:49 +0000

    Find Peek

---
+++ src/main/java/exercises/blind/FindPeekElement.java
@@ -0,0 +1,15 @@
+package exercises.blind;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class FindPeekElement {
+  public int findPeakElement(final int[] nums) {
+    return IntStream.range(0, nums.length)
+      .boxed()
+      .reduce(0, (a, b) -> {
+        if (nums[a] > nums[b]) return a;
+        return b;
+      });
+  }
+}
