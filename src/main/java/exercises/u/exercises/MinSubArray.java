1302b6035df4d88e9edfa185522a39f73e0a930b
Ricardo Gomes <rgomes@visa.com>
2024-10-30 17:02:50 +0000

    Master

---
+++ src/main/java/exercises/u/exercises/MinSubArray.java
@@ -0,0 +1,31 @@
+package exercises.u.exercises;
+
+import java.util.Arrays;
+
+public class MinSubArray {
+  public int minSubarray(int[] nums, int p) {
+    final var sum = Arrays.stream(nums).sum();
+
+    if (sum == p) {
+      return 0;
+    }
+
+    if (sum < p) {
+      return -1;
+    }
+
+    return removeSubArr(nums, sum, p);
+  }
+
+  private int removeSubArr(int[] nums, int sum, int p) {
+    final var delta = sum - p;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == delta) {
+        return 1;
+      }
+    }
+
+    return -1;
+  }
+}
