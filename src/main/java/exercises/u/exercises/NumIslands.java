ac61d38eeee8e07d7a4c15cba7f51ef441ef7a0e
Ricardo Gomes <rgomes@visa.com>
2024-11-05 22:19:38 +0000

    Number of Islands

---
+++ src/main/java/exercises/u/exercises/NumIslands.java
@@ -0,0 +1,62 @@
+package exercises.u.exercises;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class NumIslands {
+  private final static List<Point> MOVES = List.of(
+      new Point(1, 0),
+      new Point(-1, 0),
+      new Point(0, 1),
+      new Point(0, -1)
+  );
+
+
+  public int numIslands(char[][] grid) {
+    final var linkedList = fillWithIslandCoordinates(grid);
+    final var islandMap = fillWithIslandCoordinates(grid).stream()
+        .map(p -> p.x + ":" + p.y)
+        .collect(Collectors.toSet());
+
+    final var seen = new HashSet<String>();
+
+    var islands = 0;
+
+    while (!linkedList.isEmpty()) {
+      final var next = linkedList.pollFirst();
+      if (!seen.contains(next.x + ":" + next.y)) {
+        expandIsland(next, islandMap, seen);
+        islands++;
+      }
+    }
+
+    return islands;
+  }
+
+  private void expandIsland(Point next, Set<String> islandMap, Set<String> seen) {
+    if (next != null) {
+      seen.add(next.x + ":" + next.y);
+      MOVES.stream()
+          .map(p -> new Point(next.x + p.x, next.y + p.y))
+          .filter(p -> islandMap.contains(p.x + ":" + p.y))
+          .filter(p -> !seen.contains(p.x + ":" + p.y))
+          .forEach(p -> expandIsland(p, islandMap, seen));
+    }
+  }
+
+  private LinkedList<Point> fillWithIslandCoordinates(char[][] grid) {
+    final var result = new LinkedList<Point>();
+
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == '1') result.add(new Point(i, j));
+      }
+    }
+
+    return result;
+  }
+}
