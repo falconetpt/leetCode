f720dee9d31898eb59564cd74bf3b6da05277534
Ricardo Gomes <rgomes@visa.com>
2024-11-12 08:58:21 +0000

    Triplet

--- exercises/src/main/java/meetingRoom/IncreasingTriplet.java
+++ exercises/src/main/java/meetingRoom/IncreasingTriplet.java
@@ -2,30 +2,20 @@ package meetingRoom;
 
 public class IncreasingTriplet {
   public boolean increasingTriplet(int[] nums) {
-    int left = 0;
-    int right = nums.length - 1;
+    final var dp = new int[2];
+    dp[0] = Integer.MAX_VALUE;
+    dp[1] = Integer.MAX_VALUE;
 
-    return search(nums, left, right);
-  }
+    // [2,6,5,6,4,7]
+    for (final int element : nums) {
+      final var prevStart = dp[0];
+      final var prevMax = dp[1];
 
-  private boolean search(int[] nums, int left, int right) {
-    if (right - left > 2) {
-      final var leftValue = nums[left];
-      final var rightValue = nums[right];
+      if (element > prevMax) return true;
 
-      final var isGreater = rightValue > leftValue && canFind(nums, left, right, leftValue, rightValue);
+      if (element > prevStart) dp[1] = element;
 
-      return isGreater || search(nums, left + 1, right) || search(nums, left, right - 1);
-    } else {
-      return false;
-    }
-  }
-
-  private boolean canFind(int[] nums, int left, int right, int leftValue, int rightValue) {
-
-    for (int i = left + 1; i < right; i++) {
-      final var element = nums[i];
-      if (element > leftValue && element < rightValue) return true;
+      if (element < prevStart) dp[0] = element;
     }
 
     return false;
